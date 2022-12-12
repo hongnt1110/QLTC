e7e8f9cff86c23422942741644cb6a69505e650d
NAM2412 <20520643@gm.uit.edu.vn>
2022-12-12 13:58:44 +0700

    "Adding Admin View"

---
+++ QLTC/AdminDashboard.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace QLTC
+{
+    partial class AdminDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnHoaDon = new FontAwesome.Sharp.IconButton();
+            this.btnKhachHang = new FontAwesome.Sharp.IconButton();
+            this.btnNhanVien = new FontAwesome.Sharp.IconButton();
+            this.bthSanh = new FontAwesome.Sharp.IconButton();
+            this.btnTiecCuoi = new FontAwesome.Sharp.IconButton();
+            this.btnTrangChu = new FontAwesome.Sharp.IconButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnReset = new System.Windows.Forms.PictureBox();
+            this.panelChildForm = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnReset)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.White;
+            this.panelMenu.Controls.Add(this.btnHoaDon);
+            this.panelMenu.Controls.Add(this.btnKhachHang);
+            this.panelMenu.Controls.Add(this.btnNhanVien);
+            this.panelMenu.Controls.Add(this.bthSanh);
+            this.panelMenu.Controls.Add(this.btnTiecCuoi);
+            this.panelMenu.Controls.Add(this.btnTrangChu);
+            this.panelMenu.Controls.Add(this.panel2);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 631);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnHoaDon
+            // 
+            this.btnHoaDon.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHoaDon.FlatAppearance.BorderSize = 0;
+            this.btnHoaDon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHoaDon.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnHoaDon.IconChar = FontAwesome.Sharp.IconChar.MoneyBillAlt;
+            this.btnHoaDon.IconColor = System.Drawing.Color.MidnightBlue;
+            this.btnHoaDon.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnHoaDon.IconSize = 32;
+            this.btnHoaDon.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHoaDon.Location = new System.Drawing.Point(0, 430);
+            this.btnHoaDon.Name = "btnHoaDon";
+            this.btnHoaDon.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnHoaDon.Size = new System.Drawing.Size(220, 60);
+            this.btnHoaDon.TabIndex = 6;
+            this.btnHoaDon.Text = "Hóa đơn";
+            this.btnHoaDon.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHoaDon.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnHoaDon.UseVisualStyleBackColor = true;
+            this.btnHoaDon.Click += new System.EventHandler(this.btnHoaDon_Click);
+            // 
+            // btnKhachHang
+            // 
+            this.btnKhachHang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnKhachHang.FlatAppearance.BorderSize = 0;
+            this.btnKhachHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKhachHang.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnKhachHang.IconChar = FontAwesome.Sharp.IconChar.Handshake;
+            this.btnKhachHang.IconColor = System.Drawing.Color.MidnightBlue;
+            this.btnKhachHang.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnKhachHang.IconSize = 32;
+            this.btnKhachHang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnKhachHang.Location = new System.Drawing.Point(0, 370);
+            this.btnKhachHang.Name = "btnKhachHang";
+            this.btnKhachHang.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnKhachHang.Size = new System.Drawing.Size(220, 60);
+            this.btnKhachHang.TabIndex = 5;
+            this.btnKhachHang.Text = "Khách hàng";
+            this.btnKhachHang.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnKhachHang.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnKhachHang.UseVisualStyleBackColor = true;
+            this.btnKhachHang.Click += new System.EventHandler(this.btnKhachHang_Click);
+            // 
+            // btnNhanVien
+            // 
+            this.btnNhanVien.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNhanVien.FlatAppearance.BorderSize = 0;
+            this.btnNhanVien.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNhanVien.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnNhanVien.IconChar = FontAwesome.Sharp.IconChar.User;
+            this.btnNhanVien.IconColor = System.Drawing.Color.MidnightBlue;
+            this.btnNhanVien.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnNhanVien.IconSize = 32;
+            this.btnNhanVien.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNhanVien.Location = new System.Drawing.Point(0, 310);
+            this.btnNhanVien.Name = "btnNhanVien";
+            this.btnNhanVien.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnNhanVien.Size = new System.Drawing.Size(220, 60);
+            this.btnNhanVien.TabIndex = 4;
+            this.btnNhanVien.Text = "Nhân viên";
+            this.btnNhanVien.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNhanVien.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNhanVien.UseVisualStyleBackColor = true;
+            this.btnNhanVien.Click += new System.EventHandler(this.btnNhanVien_Click);
+            // 
+            // bthSanh
+            // 
+            this.bthSanh.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bthSanh.FlatAppearance.BorderSize = 0;
+            this.bthSanh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bthSanh.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.bthSanh.IconChar = FontAwesome.Sharp.IconChar.Heart;
+            this.bthSanh.IconColor = System.Drawing.Color.MidnightBlue;
+            this.bthSanh.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.bthSanh.IconSize = 32;
+            this.bthSanh.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bthSanh.Location = new System.Drawing.Point(0, 250);
+            this.bthSanh.Name = "bthSanh";
+            this.bthSanh.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.bthSanh.Size = new System.Drawing.Size(220, 60);
+            this.bthSanh.TabIndex = 3;
+            this.bthSanh.Text = "Sảnh";
+            this.bthSanh.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bthSanh.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.bthSanh.UseVisualStyleBackColor = true;
+            this.bthSanh.Click += new System.EventHandler(this.bthSanh_Click);
+            // 
+            // btnTiecCuoi
+            // 
+            this.btnTiecCuoi.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTiecCuoi.FlatAppearance.BorderSize = 0;
+            this.btnTiecCuoi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTiecCuoi.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnTiecCuoi.IconChar = FontAwesome.Sharp.IconChar.MartiniGlassEmpty;
+            this.btnTiecCuoi.IconColor = System.Drawing.Color.MidnightBlue;
+            this.btnTiecCuoi.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnTiecCuoi.IconSize = 32;
+            this.btnTiecCuoi.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTiecCuoi.Location = new System.Drawing.Point(0, 190);
+            this.btnTiecCuoi.Name = "btnTiecCuoi";
+            this.btnTiecCuoi.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnTiecCuoi.Size = new System.Drawing.Size(220, 60);
+            this.btnTiecCuoi.TabIndex = 2;
+            this.btnTiecCuoi.Text = "Tiệc cưới";
+            this.btnTiecCuoi.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTiecCuoi.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnTiecCuoi.UseVisualStyleBackColor = true;
+            this.btnTiecCuoi.Click += new System.EventHandler(this.btnTiecCuoi_Click);
+            // 
+            // btnTrangChu
+            // 
+            this.btnTrangChu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTrangChu.FlatAppearance.BorderSize = 0;
+            this.btnTrangChu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrangChu.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnTrangChu.IconChar = FontAwesome.Sharp.IconChar.BarChart;
+            this.btnTrangChu.IconColor = System.Drawing.Color.MidnightBlue;
+            this.btnTrangChu.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnTrangChu.IconSize = 32;
+            this.btnTrangChu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTrangChu.Location = new System.Drawing.Point(0, 130);
+            this.btnTrangChu.Name = "btnTrangChu";
+            this.btnTrangChu.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnTrangChu.Size = new System.Drawing.Size(220, 60);
+            this.btnTrangChu.TabIndex = 1;
+            this.btnTrangChu.Text = "Trang chủ";
+            this.btnTrangChu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTrangChu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnTrangChu.UseVisualStyleBackColor = true;
+            this.btnTrangChu.Click += new System.EventHandler(this.btnTrangChu_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnReset);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(220, 130);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Image = global::QLTC.Properties.Resources.logo;
+            this.btnReset.Location = new System.Drawing.Point(23, 22);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(172, 86);
+            this.btnReset.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnReset.TabIndex = 0;
+            this.btnReset.TabStop = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // panelChildForm
+            // 
+            this.panelChildForm.BackColor = System.Drawing.Color.White;
+            this.panelChildForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelChildForm.Location = new System.Drawing.Point(220, 0);
+            this.panelChildForm.Name = "panelChildForm";
+            this.panelChildForm.Size = new System.Drawing.Size(928, 631);
+            this.panelChildForm.TabIndex = 1;
+            // 
+            // AdminDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 631);
+            this.Controls.Add(this.panelChildForm);
+            this.Controls.Add(this.panelMenu);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "AdminDashboard";
+            this.Text = "AdminDashboard";
+            this.panelMenu.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnReset)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panel2;
+        private FontAwesome.Sharp.IconButton btnHoaDon;
+        private FontAwesome.Sharp.IconButton btnKhachHang;
+        private FontAwesome.Sharp.IconButton btnNhanVien;
+        private FontAwesome.Sharp.IconButton bthSanh;
+        private FontAwesome.Sharp.IconButton btnTiecCuoi;
+        private FontAwesome.Sharp.IconButton btnTrangChu;
+        private System.Windows.Forms.PictureBox btnReset;
+        private System.Windows.Forms.Panel panelChildForm;
+    }
+}
